1cfd21602a9aecc6cc61e76077eb03a2a73c223b
HAMPERHAMPS <149680144+HAMPERHAMPS@users.noreply.github.com>
2024-07-10 11:56:56 -0400

    Update safelife-hub.py

--- safelife-hub.py
+++ safelife-hub.py
@@ -4,10 +4,10 @@ import requests
 safever = 1.0 # Please dont change this. This lets the client know how to communicate with this.
 print(f"Running SafeLife Hub V{str(safever)}")
 mainhub = True # Please change this to false. This is letting the server know that this is the main server. Other servers use this to update.
-#if not mainhub:
-#    latestver = requests.get('https://safelifehub.hamperhamps.space/ping')
-#    if int(latestver) != safever:
-#        print("YOU ARE USING AN OLD SERVER VERSION!!! PLEASE GO TO https://github.com/HAMPERHAMPS/safelife-hub/edit/main/safelife-hub.py AND DOWNLOAD THE LATEST SERVER VERSION FOR THE CLIENT TO WORK CORRECTLY!!!")
+if not mainhub:
+    latestver = requests.get('https://safelifehub.hamperhamps.space/version')
+    if int(latestver) != safever:
+        print("YOU ARE USING AN OLD SERVER VERSION!!! PLEASE GO TO https://github.com/HAMPERHAMPS/safelife-hub/edit/main/safelife-hub.py AND DOWNLOAD THE LATEST SERVER VERSION FOR THE CLIENT TO WORK CORRECTLY!!!")
 app = Flask(__name__)
 CORS(app)
 @app.route('/', methods=['GET', 'POST'])
@@ -31,6 +31,9 @@ def proxy():
 @app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({'status': 'ok'})
+@app.route('/version', methods=['GET'])
+def ping():
+    return jsonify({'ver': 'ok'})
 
 if __name__ == '__main__':
     app.run(debug=True)
