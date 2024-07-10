e8c7fe04213ef0a57c8e5a15933dd1aa86105b99
HAMPERHAMPS <149680144+HAMPERHAMPS@users.noreply.github.com>
2024-07-10 10:43:24 -0400

    Add files via upload

---
+++ safelife-hub.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/', methods=['GET', 'POST'])
+def proxy():
+    try:
+        url = request.args.get('url')
+    
+        if url:
+            response = requests.get(url)
+            return jsonify({
+                'status_code': response.status_code,
+                'content': response.text
+            })
+
+    except:
+        return jsonify({
+            'status_code': 1,
+            'content': """<p style="color: red;">NYOOOOOoOooOOOo that site doesnt exist.</p>"""
+        })
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
